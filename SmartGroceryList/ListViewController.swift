e121c610bbee3bbdbbac79483a9b946dffe3cf1d
Meghan Kane <meghan.kane@soundcloud.com>
2017-07-28 21:56:19 +0200

    Initial 2 tabbed application

---
+++ SmartGroceryList/ListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewController.swift
+//  SmartGroceryList
+//
+//  Created by Meghan Kane on 7/28/17.
+//  Copyright © 2017 Meghan Kane. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
---
+++ SmartGroceryList/RecognizerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecognizerViewController.swift
+//  SmartGroceryList
+//
+//  Created by Meghan Kane on 7/28/17.
+//  Copyright © 2017 Meghan Kane. All rights reserved.
+//
+
+import UIKit
+
+class RecognizerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
