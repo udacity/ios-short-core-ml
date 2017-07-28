1d9d2b893921a6544c3ad189d372d22c8f673b9c
Meghan Kane <meghan.kane@soundcloud.com>
2017-07-28 23:42:26 +0200

    Show placeholder items in list

---
+++ SmartGroceryList/GroceryItemTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  GroceryItemTableViewCell.swift
+//  SmartGroceryList
+//
+//  Created by Meghan Kane on 7/28/17.
+//  Copyright © 2017 Meghan Kane. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItemTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var quantityLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
