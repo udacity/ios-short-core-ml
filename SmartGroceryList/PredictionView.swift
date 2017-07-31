d6b92e10ef853084eaadb6f13d4aeb11cc18cd8d
Meghan Kane <meghan.kane@soundcloud.com>
2017-07-31 12:04:45 +0200

    Cleanup UI

---
+++ SmartGroceryList/PredictionView.swift
@@ -0,0 +1,21 @@
+//
+//  PredictionView.swift
+//  SmartGroceryList
+//
+//  Created by Meghan Kane on 7/31/17.
+//  Copyright © 2017 Meghan Kane. All rights reserved.
+//
+
+import UIKit
+
+class PredictionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
